22ff270340ccf5ec15dfa15d787bfc3428ed1e7a
gtheler <gtheler@tom.seamplex>
2016-12-06 17:49:55 -0300

    fixed get.sh and roessler.was

--- get.sh
+++ get.sh
@@ -3,27 +3,37 @@
 
 #
 # This script is meant for quick & easy install via:
-#   'curl -sSL https://get.docker.com/ | sh'
+#   'curl https://www.seamplex.com/wasora/get.sh | sh'
 # or:
-#   'wget -qO- https://get.docker.com/ | sh'
+#   'wget -O- https://www.seamplex.com/wasora/get.sh | sh'
 
 # loosely based on docker's get.sh script at https://get.docker.com/
 # jeremy theler
+
+name=wasora
 
 # check for needed tools
 command_exists() {
   command -v "$@" > /dev/null 2>&1
 }
 
+
 # step 0: needed commands
 # if wasora-suite does not exist, create
-echo -n "0. checking pre-requisistes..." | tee -a get.log
+if test "`whoami`" = "root"; then
+  echo "error: do not run me as root"
+  exit 1
+fi
+
+
+rm -f get.log
+echo -n "0. checking pre-requisistes..." | tee -a get.log; echo >> get.log
 for i in m4 autoconf xargs gcc git make makeinfo; do
  if ! command_exists $i; then
   echo
   echo "error: $i not installed"
   echo "install the following packages using your favorite package manager:"
-  echo "m4 autoconf gcc mercurial make"
+  echo "m4 autoconf gcc git make"
   exit 1
  fi
 done
@@ -42,36 +52,36 @@ echo "ok!"
 # step 1: wasora-suite directory
 # if wasora-suite does not exist, create
 if test ! -d ./wasora-suite; then
-  echo -n "1. creating wasora-suite subdir..." | tee -a get.log
+  echo -n "1. creating wasora-suite subdir..." | tee -a get.log; echo >> get.log
   mkdir wasora-suite
   echo "ok!"
 else
-  echo "1. wasora-suite subdir exists, good!"  | tee -a get.log
+  echo "1. wasora-suite subdir exists, good!"  | tee -a get.log; echo >> get.log
 fi
   
 cd wasora-suite
 
 # step 2: clone or update repo
-if test ! -d ./wasora; then
-  echo -n "2. cloning wasora repository..."  | tee -a ../get.log
-  git clone https://bitbucket.org/seamplex/wasora.git >> ../get.log
-  cd wasora
+if test ! -d ./${name}; then
+  echo -n "2. cloning ${name} repository..."  | tee -a ../get.log; echo >> get.log
+  git clone https://bitbucket.org/seamplex/${name}.git >> ../get.log; echo >> get.log
+  cd ${name}
   echo "ok!"
 else
-  echo -n "2. pulling and updating wasora repository..."  | tee -a ../get.log
-  cd wasora
-  git pull  >> ../../get.log
+  echo -n "2. pulling and updating ${name} repository..."  | tee -a ../get.log; echo >> get.log
+  cd ${name}
+  git pull  >> ../../get.log; echo >> get.log
   echo "ok!"
 fi
 
 
 # step 3: bootstrap repo
-echo -n "3. bootstrapping wasora source tree..."  | tee -a ../../get.log
-./autogen.sh 2>&1 >> ../../get.log
+echo -n "3. bootstrapping ${name} source tree..."  | tee -a ../../get.log; echo >> get.log
+./autogen.sh 2>&1 >> ../../get.log; echo >> get.log
 echo "ok!"
 
 # step 4: check for GSL
-echo -n "4. checking for GSL..." | tee -a ../../get.log
+echo -n "4. checking for GSL..." | tee -a ../../get.log; echo >> get.log
 cat << EOF > test.c
 #include <gsl/gsl_version.h>
 
@@ -89,7 +99,7 @@ if test ${nodynamicgsl} -eq 1; then
  echo
  echo "The development version of GSL is not installed, you may:"
  echo "  a. let me try to install the package for yourself (needs sudo)"
- echo "  b. ask wasora to try to download  and compile the GSL source"
+ echo "  b. ask ${name} to try to download  and compile the GSL source"
  echo "  c. abort this script now, manually install package libgsl0-dev and re-run"
  echo -n "What do you want to do? (a/b/c) "
  
@@ -110,8 +120,8 @@ if test ${nodynamicgsl} -eq 1; then
      exit 1
     fi;;
   "b")
-    echo "ok, I will ask configure to download and compile it" | tee -a ../../get.log
-    echo "note that step number 5 may take some time" | tee -a ../../get.log
+    echo "ok, I will ask configure to download and compile it" | tee -a ../../get.log; echo >> get.log
+    echo "note that step number 5 may take some time" | tee -a ../../get.log; echo >> get.log
     configflag="--enable-download-gsl"
     ;;
   *)
@@ -121,34 +131,34 @@ if test ${nodynamicgsl} -eq 1; then
  
 else
  configflag=""
- echo "already installed!" | tee -a ../../get.log
+ echo "already installed!" | tee -a ../../get.log; echo >> get.log
 fi
 
 
 # step 5. configure
-echo -n "5. configuring wasora..."  | tee -a ../../get.log
-./configure ${configflag} >> ../../get.log
+echo -n "5. configuring ${name}..."  | tee -a ../../get.log; echo >> get.log
+./configure ${configflag} >> ../../get.log; echo >> get.log
 echo "ok!"
 
 # step 6. compile
-echo -n "6. compiling wasora..."  | tee -a ../../get.log
-make >> ../../get.log
+echo -n "6. compiling ${name}..."  | tee -a ../../get.log; echo >> get.log
+make >> ../../get.log; echo >> get.log
 echo "ok!"
 
 # step 7. link to $HOME/bin
-echo -n "7. linking $HOME/bin/wasora to the recently compiled binary..."
+echo -n "7. linking $HOME/bin/${name} to the recently compiled binary..."
 if test ! -d $HOME/bin; then
   mkdir -p $HOME/bin
 fi
-rm -rf $HOME/bin/wasora
-ln -s $PWD/wasora $HOME/bin/wasora
+rm -rf $HOME/bin/${name}
+ln -s $PWD/${name} $HOME/bin/${name}
 echo "ok!"
 
 # step 8. add $HOME/bin to the path
 if test -z "`echo $PATH | grep $HOME/bin`"; then
   echo -n "8. adding $HOME/bin to the path in ~/.bashrc"
   cat >> $HOME/.bashrc << EOF
-# added by wasora get.sh
+# added by ${name} get.sh
 export PATH=\$PATH:\$HOME/bin
 EOF
   export PATH=\$PATH:\$HOME/bin
@@ -161,11 +171,12 @@ cd ../..
 
 # test if it worked!
 echo
-if command_exists wasora; then
-  echo "congratulations! wasora compiled and installed correctly!"
-  echo "type 'wasora' (without the quotes) if you do not believe me."
+if command_exists ${name}; then
+  echo "congratulations! ${name} compiled and installed correctly!"
+  echo "type '${name}' (without the quotes) if you do not believe me."
 else
-  echo "something went wrong as wasora does not run :-("
-  echo "share your get.log file with the mailing list for help"
+  echo "something went wrong as ${name} does not run :-("
+  echo "share your get.log file with the mailing list for help."
+  echo "see <https://groups.google.com/a/seamplex.com/forum/#!forum/wasora>"
 fi
 

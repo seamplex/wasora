edc461a2b2a7869f9c71436bfe478997182193df
iepale <pablo@pablo.casa>
2017-10-28 18:05:04 -0300

    Definitions of HEXAHEDRON 27 and 20 in wasora.h and src/mesh/hexahedron27.c added.

---
+++ src/mesh/hexahedron27.c
@@ -0,0 +1,290 @@
+/*------------ -------------- -------- --- ----- ---   --       -            -
+ *  wasora's mesh-related hexahedron element routines
+ *
+ *  Copyright (C) 2014--2017 jeremy theler
+ *
+ *  This file is part of wasora.
+ *
+ *  wasora is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  wasora is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with wasora.  If not, see <http://www.gnu.org/licenses/>.
+ *------------------- ------------  ----    --------  --     -       -         -
+ */
+#include <wasora.h>
+
+#include <math.h>
+#include <gsl/gsl_linalg.h>
+
+
+// --------------------------------------------------------------
+// hexahedro de 27 nodos
+// --------------------------------------------------------------
+int mesh_twentyseven_node_hexaedron_init(void) {
+  
+  element_type_t *element_type;
+  gauss_t *gauss;
+
+  element_type = &wasora_mesh.element_type[ELEMENT_TYPE_HEXAHEDRON27];
+  element_type->name = strdup("hexahedron27");
+  element_type->id = ELEMENT_TYPE_HEXAHEDRON27;
+  element_type->dim = 3;
+  element_type->order = 2;
+  element_type->nodes = 27;
+  element_type->faces = 6;
+  element_type->nodes_per_face = 9;
+  element_type->h = mesh_twentyseven_node_hexahedron_h;
+  element_type->dhdr = mesh_twentyseven_node_quad_dhdr;
+  element_type->point_in_element = mesh_point_in_hexahedron;   //Capas que cambia,
+  element_type->element_volume = mesh_hexahedron_vol;//Capas que cambia,
+
+  // dos juegos de puntos de gauss
+  element_type->gauss = calloc(2, sizeof(gauss_t));
+  
+  // el primero es el default
+  // ---- ocho puntos de Gauss sobre el elemento unitario ----  
+    gauss = &element_type->gauss[GAUSS_POINTS_CANONICAL];
+    mesh_alloc_gauss(gauss, element_type, 8);
+
+    gauss->w[0] = 8 * 1.0/8.0;
+    gauss->r[0][0] = -1.0/M_SQRT3;
+    gauss->r[0][1] = -1.0/M_SQRT3;
+    gauss->r[0][2] = -1.0/M_SQRT3;
+
+    gauss->w[1] = 8 * 1.0/8.0;
+    gauss->r[1][0] = +1.0/M_SQRT3;
+    gauss->r[1][1] = -1.0/M_SQRT3;
+    gauss->r[1][2] = -1.0/M_SQRT3;
+
+    gauss->w[2] = 8 * 1.0/8.0;
+    gauss->r[2][0] = +1.0/M_SQRT3;
+    gauss->r[2][1] = +1.0/M_SQRT3;
+    gauss->r[2][2] = -1.0/M_SQRT3;
+
+    gauss->w[3] = 8 * 1.0/8.0;
+    gauss->r[3][0] = -1.0/M_SQRT3;
+    gauss->r[3][1] = +1.0/M_SQRT3;
+    gauss->r[3][2] = -1.0/M_SQRT3;
+
+    gauss->w[4] = 8 * 1.0/8.0;
+    gauss->r[4][0] = -1.0/M_SQRT3;
+    gauss->r[4][1] = -1.0/M_SQRT3;
+    gauss->r[4][2] = +1.0/M_SQRT3;
+
+    gauss->w[5] = 8 * 1.0/8.0;
+    gauss->r[5][0] = +1.0/M_SQRT3;
+    gauss->r[5][1] = -1.0/M_SQRT3;
+    gauss->r[5][2] = +1.0/M_SQRT3;
+
+    gauss->w[6] = 8 * 1.0/8.0;
+    gauss->r[6][0] = +1.0/M_SQRT3;
+    gauss->r[6][1] = +1.0/M_SQRT3;
+    gauss->r[6][2] = +1.0/M_SQRT3;
+
+    gauss->w[7] = 8 * 1.0/8.0;
+    gauss->r[7][0] = -1.0/M_SQRT3;
+    gauss->r[7][1] = +1.0/M_SQRT3;
+    gauss->r[7][2] = +1.0/M_SQRT3;
+  
+    mesh_init_shape_at_gauss(gauss, element_type);
+    
+  // ---- un punto de Gauss sobre el elemento unitario ----  
+    gauss = &element_type->gauss[GAUSS_POINTS_SINGLE];
+    mesh_alloc_gauss(gauss, element_type, 1);
+  
+    gauss->w[0] = 8 * 1.0;
+    gauss->r[0][0] = 0;
+    gauss->r[0][1] = 0;
+
+    mesh_init_shape_at_gauss(gauss, element_type);  
+  
+  return WASORA_RUNTIME_OK;
+}
+
+double mesh_twentyseven_node_hexahedron_h(int j, gsl_vector *gsl_r) {
+  double r;
+  double s;
+  double t;
+
+  r = gsl_vector_get(gsl_r, 0);
+  s = gsl_vector_get(gsl_r, 1);
+  t = gsl_vector_get(gsl_r, 2);
+
+  switch (j) {
+    case 0:
+      return 0.25*(1-r)*(1-s);
+      break;
+    case 1:
+      return 0.25*(1+r)*(1-s);
+      break;
+    case 2:
+      return 0.25*(1+r)*(1+s);
+      break;
+    case 3:
+      return 0.25*(1-r)*(1+s);
+      break;
+    case 4:
+      break;
+    case 5:
+      break;
+    case 6:
+      break;
+    case 7:
+      break;
+    case 8:
+      break;
+    case 9:
+      break;
+    case 10:
+      break;
+    case 11:
+      break;
+    case 12:
+      break;
+    case 13:
+      break;
+    case 14:
+      break;
+    case 15:
+      break;
+    case 16:
+      break;
+    case 17:
+      break;
+    case 18:
+      break;
+    case 19:
+      break;
+    case 20:
+      break;
+    case 21:
+      break;
+    case 22:
+      break;
+    case 23:
+      break;
+    case 24:
+      break;
+    case 25:
+      break;
+    case 26:
+      break;
+  }
+
+  return 0;
+
+}
+
+double mesh_four_node_quad_dhdr(int j, int m, gsl_vector *gsl_r) {
+  double r;
+  double s;
+
+  r = gsl_vector_get(gsl_r, 0);
+  s = gsl_vector_get(gsl_r, 1);
+
+  switch(j) {
+    case 0:
+      if (m == 0) {
+        return -0.25*(1-s);
+      } else {
+        return -0.25*(1-r);
+      }
+      break;
+    case 1:
+      if (m == 0) {
+        return 0.25*(1-s);
+      } else {
+        return -0.25*(1+r);
+      }
+      break;
+    case 2:
+      if (m == 0) {
+        return 0.25*(1+s);
+      } else {
+        return 0.25*(1+r);
+      }
+      break;
+    case 3:
+      if (m == 0) {
+        return -0.25*(1+s);
+      } else {
+        return 0.25*(1-r);
+      }
+      break;
+
+  }
+
+  return 0;
+
+
+}
+
+
+
+int mesh_point_in_quadrangle(element_t *element, const double *x) {
+
+/*
+  double z1, z2, z3;
+
+  z1 = mesh_subtract_cross_2d(element->node[0]->x, element->node[1]->x, x);
+  z2 = mesh_subtract_cross_2d(element->node[1]->x, element->node[2]->x, x);
+  z3 = mesh_subtract_cross_2d(element->node[2]->x, element->node[0]->x, x);
+  
+  if ((GSL_SIGN(z1) == GSL_SIGN(z2) && GSL_SIGN(z2) == GSL_SIGN(z3)) ||
+      (fabs(z1) < 1e-4 && GSL_SIGN(z2) == GSL_SIGN(z3)) ||
+      (fabs(z2) < 1e-4 && GSL_SIGN(z1) == GSL_SIGN(z3)) ||          
+      (fabs(z3) < 1e-4 && GSL_SIGN(z1) == GSL_SIGN(z2)) ) {
+    return 1;
+  }
+  
+  
+  z1 = mesh_subtract_cross_2d(element->node[0]->x, element->node[2]->x, x);
+  z2 = mesh_subtract_cross_2d(element->node[2]->x, element->node[3]->x, x);
+  z3 = mesh_subtract_cross_2d(element->node[3]->x, element->node[0]->x, x);
+
+  if ((GSL_SIGN(z1) == GSL_SIGN(z2) && GSL_SIGN(z2) == GSL_SIGN(z3)) ||
+      (fabs(z1) < 1e-4 && GSL_SIGN(z2) == GSL_SIGN(z3)) ||
+      (fabs(z2) < 1e-4 && GSL_SIGN(z1) == GSL_SIGN(z3)) ||          
+      (fabs(z3) < 1e-4 && GSL_SIGN(z1) == GSL_SIGN(z2)) ) {
+    return 1;
+  }
+  
+  return 0;
+*/
+
+  int i, j;
+
+  element_t triang;
+
+  triang.type = &wasora_mesh.element_type[ELEMENT_TYPE_TRIANGLE];
+  triang.node = calloc(triang.type->nodes, sizeof(node_t *));
+
+  for (i = 0; i < element->type->faces; i++) {
+    for (j = 0; j < triang.type->faces; j++) {
+      triang.node[j] = element->node[(i+j) % element->type->faces];
+    }
+    if (mesh_point_in_triangle(&triang, x)) {
+      free(triang.node);
+      return 1;
+    }
+  }
+
+  free(triang.node);
+
+  return 0;
+}
+
+double mesh_quad_vol(element_t *element) {
+  
+  return 0.5*(fabs(mesh_subtract_cross_2d(element->node[0]->x, element->node[1]->x, element->node[2]->x)) +
+              fabs(mesh_subtract_cross_2d(element->node[2]->x, element->node[3]->x, element->node[0]->x)) );
+}
+

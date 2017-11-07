5de0f0859aa4cf2dec408ebe8758168e27929732
iepale <pablo@pablo.casa>
2017-11-07 19:55:38 -0300

    quad9 inited.

--- src/mesh/element.c
+++ src/mesh/element.c
@@ -160,6 +160,7 @@ int wasora_mesh_element_types_init(void) {
   
   // quadrangle ----------------------------------------------------------------
   mesh_four_node_quadrangle_init();
+  mesh_nine_node_quadrangle_init();
   
   // tetrahedra  ---------------------------------------------------------------
   mesh_four_node_tetrahedron_init();
@@ -180,7 +181,7 @@ int wasora_mesh_element_types_init(void) {
   element_type->id = ELEMENT_TYPE_PYRAMID;
   element_type->nodes = 0;
 
-  wasora_mesh.element_type[10].name = strdup("quad9");
+//  wasora_mesh.element_type[10].name = strdup("quad9");
   
 //  wasora_mesh.element_type[12].name = strdup("hexa27");
   wasora_mesh.element_type[13].name = strdup("prism18");
